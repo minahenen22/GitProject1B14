34a099181801048c59d24564688f110334363234
minahenen <w.minahanna@gmail.com>
2022-12-17 11:00:46 -0500

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("First Version Is Complete");
+        System.out.println("2nd  Version Is Complete");
     }
 }
